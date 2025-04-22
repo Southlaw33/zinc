2aaf69898475e1b8b4b46c5f8c732f1eab6faf78
Atchutha Rao T A <atchutha57@gmail.com>
2025-04-22 12:16:21 +0530

    Add ClientPostsList component to fetch and display posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,14 +1,17 @@
 import React, { Suspense } from "react";
 import PostLists from "./components/PostLists";
+import ClientPostsList from "./components/ClientPostsList";
 
 const PostsPage = () => {
   return (
     <div>
       <h1>Posts</h1>
-      <Suspense fallback={
-        <div className="container mx-auto text-blue-700">Loading posts</div>
-      }>
-        <PostLists />
+      <Suspense
+        fallback={
+          <div className="container mx-auto text-blue-700">Loading posts</div>
+        }
+      >
+        <ClientPostsList />
       </Suspense>
     </div>
   );
